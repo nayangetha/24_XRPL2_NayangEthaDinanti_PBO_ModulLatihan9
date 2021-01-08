1f2d1c377476190ea49fa7264ae3a7f285ec66f3
unknown <nayang_etha_29rpl@student.smktelkom-mlg.sch.id>
2021-01-08 19:08:36 +0700

    first commit

---
+++ src/latihan_9/Latihan_9.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_9;
+
+/**
+ *
+ * @author MOKLET1
+ */
+public class Latihan_9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Semangat Pagi!!");
+        //Perulangan
+        for(int i=1; i<=5; i++){
+            System.out.println("Pagi!!");
+        }
+
+    }
+    
+}
